9975b13c9a0d7644801533b5e93ce6237f3b4445
Matthew Hume <mtthwhm@gmail.com>
2025-10-30 10:39:11 -0400

    add simple testsuites main program

---
+++ testsuites/main.c
@@ -0,0 +1,14 @@
+#include <SCSDK.h>
+#include <stdio.h>
+
+int main(int argc, char** argv){
+  // Parse command line arguments
+
+  // Run test cases
+
+  // Summarize results
+
+  // Handle any errors
+  return 0;
+}
+  
