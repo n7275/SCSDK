f3d9b695e934bfeecf0f1d853a59e5f5daa57859
Matthew Hume <mtthwhm@gmail.com>
2026-06-05 17:33:00 -0400

    More types and structures

--- include/SCSDK.h
+++ include/SCSDK.h
@@ -21,26 +21,26 @@
   along with SCSDK; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-  See <URL>
+  See http://mwhume.space/scsdk
   for more details.
 
 *******************************************************************************/
 
-#ifndef SCSDK
+ifndef SCSDK
 #define SCSDK
 
-enum entityType{
+typedef enum scsdk_entity_type{
   ELECTRIC_PUMP,
   TANK,
   PIPE
-};
+} scsdk_entity_type_t;
 
 //Main entity type
-typedef struct {
+typedef struct entity{
   int ID;
   entityType type;
   int flags;
-} entity;
+} entity_t;
 
 // Structure containing all entities
 typedef struct {
@@ -55,15 +55,45 @@ int SCSDK_Update_Systems(double dt, SCSDKInstance* pSCSDK){
 }
 
 /*****************************************************************************
+ Linkage Table Definition
+******************************************************************************/
+
+typedef union scsdk_linkage_property{
+  int direction;
+  double amount;
+  bool is_active;
+  long l_int_property;
+  double d_double_property;
+} scsdk_linkage_property_t;
+
+typedef enum scsdk_linkage_type{
+  is_a_member_of;
+  
+} scsdk_linkage_type_t;
+
+typedef struct scsdk_linkage_table_struct{
+  entity* pEntA;
+  entity* pEntB;
+  scsdk_linkage_type_t connection_type;
+  scsdk_linkage_property_t connection_property;
+}scsdk_linkage_table_struct_t;
+
+
+/*****************************************************************************
  All component types defined below.
 ******************************************************************************/
 
+
+
+
+//bad example, remove
+/*
 #define MAX_ELEC_I_O = 4;
 typedef struct {
   int ID;
   double inputVoltage[MAX_ELEC_I_O];
   double outputVoltage[MAX_ELEC_I_O];
 } electricalComponent;
-
+*/
   
 #endif
